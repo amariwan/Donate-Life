bfe169c2ae13e3372d59ff97b89149cd4814ef6e
Aland Mariwan <59203362+amariwan@users.noreply.github.com>
2021-12-09 12:26:52 +0100

    Donate Life 🩸

---
+++ script/home.js
@@ -0,0 +1,31 @@
+var people = document.getElementById("name");
+var email = document.getElementById("email");
+var type = document.getElementById("type");
+
+var list = [];
+
+const Confirm = () => {
+  event.preventDefault();
+
+  var object = { name: people.value, email: email.value, type: type.value };
+
+  if (object.name && object.email && object.type) {
+    list.push(object);
+
+    var ul = document.getElementById("list");
+
+    var li = document.createElement("li");
+    var span = document.createElement("span");
+    var text = document.createTextNode(`( ${object.type} ) ${object.name}`);
+    
+    ul.appendChild(li);
+    li.appendChild(span);
+    span.appendChild(text);
+    
+    people.value = "";
+    email.value = "";
+    type.value = "";
+  } else {
+    document.getElementById("input_text").textContent = "This just got added";
+  }
+};
